5b0f3c4548076dd4c7e7b3bd5485bfca9d012b73
Bryant Deters <bryantdeters@gmail.com>
2025-02-26 16:37:56 -0600

    extensions fix

--- configuration.nix
+++ configuration.nix
@@ -162,34 +162,34 @@
       DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
       DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
       SearchBar = "unified"; # alternative: "separate"
-    };
 
-    # ---- EXTENSIONS ----
-    # Check about:support for extension/add-on ID strings.
-    # Valid strings for installation_mode are "allowed", "blocked",
-    # "force_installed" and "normal_installed".
-    ExtensionSettings = {
-      "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
-      # uBlock Origin:
-      "uBlock0@raymondhill.net" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-        installation_mode = "force_installed";
-      };
-      # Privacy Badger:
-      "jid1-MnnxcxisBPnSXQ@jetpack" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
-        installation_mode = "force_installed";
-      };
+      # ---- EXTENSIONS ----
+      # Check about:support for extension/add-on ID strings.
+      # Valid strings for installation_mode are "allowed", "blocked",
+      # "force_installed" and "normal_installed".
+      ExtensionSettings = {
+        "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+        # uBlock Origin:
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        # Privacy Badger:
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+        };
 
-      # VivalidFox:
-      "@vivaldi-fox" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/file/4023673/vivaldifox-3.6.xpi";
-        installation_mode = "force_installed";
-      };
-      # Facebook Container:
-      "@contain-facebook" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/file/4141092/facebook_container-2.3.11.xpi";
-        installation_mode = "force_installed";
+        # VivalidFox:
+        "@vivaldi-fox" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4023673/vivaldifox-3.6.xpi";
+          installation_mode = "force_installed";
+        };
+        # Facebook Container:
+        "@contain-facebook" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4141092/facebook_container-2.3.11.xpi";
+          installation_mode = "force_installed";
+        };
       };
     };
 
