972ee138bffca98e4cfd021c53fed8f432e1deeb
Bryant Deters <bryantdeters@gmail.com>
2025-03-24 17:52:23 -0500

    17 current  2025-03-24 17:49:47  24.11.716027.f0946fa5f1fb  6.13.7                          *

--- configuration.nix
+++ configuration.nix
@@ -56,6 +56,7 @@ in
   };
 
   # Testing for game compatibility
+  hardware.graphics.enable32Bit = true;
   hardware.graphics.extraPackages = with pkgs; [ amdvlk ];
   hardware.graphics.extraPackages32 = with pkgs.driversi686Linux; [ amdvlk ];
 
