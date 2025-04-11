7ab2efa9d4f1d516e20848eeb91faaaf20ab197d
Bryant Deters <bryantdeters@gmail.com>
2025-04-11 14:52:47 -0500

    45 current  2025-04-11 14:52:46  24.11.716716.7819a0d29d1d  6.13.9                          *

--- configuration.nix
+++ configuration.nix
@@ -356,18 +356,18 @@ in
     extraCompatPackages = [ pkgs.proton-ge-bin ];
     gamescopeSession.enable = true;
     package = pkgs.steam.override {
-      # extraEnv = {
+      extraEnv = {
       #   SDL_VIDEODRIVER = "windows";
-      #   ENABLE_VKBASALT = 0;
+        ENABLE_VKBASALT = 1;
       #   PROTON_HIDE_NVIDIA_GPU = 0;
       #   PROTON_ENABLE_NVAPI = 1;
       #   PROTON_ENABLE_NGX_UPDATER = 1;
-      #   PROTON_USE_D9VK = 1;
-      #   PROTON_USE_VKD3D = 1;
-      #   DXVK_ASYNC = 1;
+        PROTON_USE_D9VK = 1;
+        PROTON_USE_VKD3D = 1;
+        DXVK_ASYNC = 1;
       #   __GL_VRR_ALLOWED = 1;
-      #   PROTON_NO_ESYNC = 1;
-      # };
+        PROTON_NO_ESYNC = 1;
+      };
     };
   };
   programs.gamemode.enable = true;
