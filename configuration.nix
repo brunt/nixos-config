ca832441876a7425b36a2a6ed5fe1dbec053e886
Bryant Deters <bryantdeters@gmail.com>
2025-07-17 16:17:21 -0500

    104 current  2025-07-17 16:17:19  25.05.806304.dfcd5b901dba  6.15.6                          *

--- configuration.nix
+++ configuration.nix
@@ -114,6 +114,13 @@ in
     LC_TIME = "en_US.UTF-8";
   };
 
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-chinese-addons
+    ];
+  };
+
   # RAM directories
   fileSystems."/home/b/Downloads" = {
     device = "none";
